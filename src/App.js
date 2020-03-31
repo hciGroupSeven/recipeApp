af2de2b192d981e5d167a44730a88d87997a7379
oliver hodge <oliverhodge2@ufl.edu>
2020-03-31 16:00:54 -0400

    columns

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Container fluid>
+          <Row>
+            <Col sm={2}>Recipe App</Col>
+            <Col sm={10}></Col>
+          </Row>
+        </Container>
       </header>
+      <Row>
+        <Col sm={3} className='leftCol'>
+          col 1
+        </Col>
+        <Col sm={6} className='midCol'>
+          col 2
+        </Col>
+        <Col sm={3} className='rightCol'>
+          col 3
+        </Col>
+      </Row>
     </div>
   );
 }
